69085bdb9c33b27c780c2dc21e75680f3538169f
kohei240155 <k.pino519@gmail.com>
2024-06-16 18:17:13 +0900

    Add new file

---
+++ src/main/java/com/example/backendtodoapp/domain/repository/TodoRepository.java
@@ -0,0 +1,8 @@
+package com.example.backendtodoapp.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.backendtodoapp.domain.model.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
