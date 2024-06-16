b227bbf096803f9913716dc15b2d81c659292fd5
kohei240155 <k.pino519@gmail.com>
2024-06-16 16:30:17 +0900

    Initial commit

---
+++ src/main/java/com/example/backendtodoapp/BackendTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendtodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTodoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backend_todo_app/BackendTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_todo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
